7fc4540cfd9813c8666293859012f0b627eb4999
Elias Cohenca <elias.cohenca@autodesk.com>
2020-01-20 12:15:47 +0200

    Add util to read prompt

---
+++ readPrompt.py
@@ -0,0 +1,14 @@
+# Prints the first line when connecting to the printer
+# Use it to find the printer's prompt (if any)
+# If it seems to get stuck, there's probably no prompt,
+# in which case just press Ctrl-C to exit.
+import sys
+import telnetlib
+
+ip = "x.x.x.x"  # Replace with printer's IP address
+try:
+    tn = telnetlib.Telnet(ip, 23)
+    line = tn.read_until("\n") # Read first line
+    print(line)
+except:
+    sys.exit("Telnet failure")
